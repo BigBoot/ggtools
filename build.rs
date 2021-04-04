7473b09c6ee3a61d9c8dc1c5dfb875a3cbaffd22
Marco Kirchner <kirchnermarco@gmail.com>
2021-04-04 14:31:07 +0200

    update dependencies & remove obsolte discord_bot

--- build.rs
+++ build.rs
@@ -1,5 +1,12 @@
-use built::write_built_file;
+use built::write_built_file_with_opts;
+use std::{env, path::Path};
 
 fn main() {
-    write_built_file().expect("Failed to acquire build-time information");
+    let mut opts = built::Options::default();
+    opts.set_git(true);
+    opts.set_time(true);
+
+    let src = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dst = Path::new(&env::var("OUT_DIR").unwrap()).join("built.rs");
+    write_built_file_with_opts(&opts, &Path::new(&src), &dst).expect("Failed to acquire build-time information");
 }
--- src/patcher.rs
+++ src/patcher.rs
@@ -1,12 +1,10 @@
 #![windows_subsystem = "windows"]
 mod app_info;
 
-use futures::executor::block_on;
-
 fn main() {
     app_info::init();
 
     let config = rgcp_common::config::Config::load();
 
-    block_on(rgcp_patcher::run(config));
+    rgcp_patcher::run(config);
 }
