99cbe351e3ab4b817f4a18a2e36bc7df292f8f80
Marco Kirchner <Marco.Kirchner@aracom.de>
2021-04-06 20:05:10 +0200

    improve patcher

--- patcher/src/lib.rs
+++ patcher/src/lib.rs
@@ -1,24 +1,5 @@
-use iced::{
-    button,
-    executor,
-    window::Settings as Window,
-    text_input,
-    Align,
-    Application,
-    Button,
-    Clipboard,
-    Checkbox,
-    Color,
-    Column,
-    Command,
-    Container,
-    Element,
-    Length,
-    Row,
-    Settings,
-    Text,
-    TextInput,
-};
+use chrono::Utc;
+use iced::{Align, Application, Button, Clipboard, Column, Command, Container, Element, Image, Length, PickList, Row, Settings, Space, Text, TextInput, button, executor, pick_list, text_input, window::Settings as Window, image::Handle};
 use nfd::Response;
 use qbsdiff::Bspatch;
 use rgcp_common::config::Config;
@@ -38,23 +19,23 @@ pub type PatchResult = ::std::result::Result<(), PatchError>;
 #[derive(Default)]
 struct Patcher {
     target_path: String,
-    patches: Vec<(Patch, bool)>,
-    status_msg: String,
-    status_color: [f32; 4],
+    patches: Vec<Patch>,
     file_select_input: text_input::State,
     browse_button: button::State,
     patch_button: button::State,
+    patch_selector: pick_list::State<Patch>,
+    selected_patch: Patch,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     BrowsePressed,
     FileSelectInputChanged(String),
-    PatchSelected(String, bool),
+    PatchSelected(Patch),
     PatchPressed,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, Default)]
 struct Patch {
     id: String,
     description: String,
@@ -67,53 +48,60 @@ impl Patch {
     }
 }
 
+impl std::fmt::Display for Patch {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.description)
+    }
+}
+
+impl std::cmp::PartialEq for Patch {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
 impl Patcher {
-    fn set_status(&mut self, msg: &str, color: Color) {
-        self.status_msg = msg.to_owned();
-        self.status_color = [color.r, color.g, color.b, color.a];
+    fn set_status(&mut self, msg: &str) {
+        eprintln!("{}", &msg);
     }
 
-    fn apply_patches(&mut self) -> PatchResult {
-        let patches = self
-            .patches
-            .iter()
-            .filter_map(|(patch, is_checked)| {
-                if *is_checked {
-                    Some(patch.clone())
-                }
-                else {
-                    None
-                }
-            })
-            .collect::<Vec<Patch>>();
-
-
+    fn apply_patch(&mut self) -> PatchResult {
+        
         let target = std::path::PathBuf::from(self.target_path.clone());
 
-        self.set_status(&format!("Loading file {}", &self.target_path), Color::BLACK);
-        let mut data = std::fs::read(target.clone())?;
-
-        for patch in &patches {
-            self.set_status(&format!("Applying patch {}", &patch.description), Color::BLACK);
-
-            let patcher = Bspatch::new(&patch.diff)?.buffer_size(4096);
-            let mut target = Vec::new();
-
-            patcher.apply(&data, std::io::Cursor::new(&mut target))?;
-
-            data = target;
+        if !target.exists() {
+            return Err(PatchError("The selected file does not exists.".to_owned()));
         }
 
-        let out_file = std::path::PathBuf::from(target.clone())
+        self.set_status(&format!("Loading file {}", &self.target_path));
+        let data = std::fs::read(target.clone())?;
+
+        self.set_status(&format!("Applying patch {}", &self.selected_patch.description));
+
+        let patcher = Bspatch::new(&self.selected_patch.diff)?.buffer_size(4096);
+        let mut result = Vec::new();
+
+        patcher.apply(&data, std::io::Cursor::new(&mut result))?;
+
+        let backup = std::path::PathBuf::from(target.clone())
             .with_file_name(
-                target.file_stem().and_then(|s| s.to_str()).map(|s| format!("{}-Patched", s)).unwrap_or(format!("")),
+                target.file_stem().and_then(|s| s.to_str()).map(|s| format!("{}_{}", s, Utc::now().format("%Y%m%d_%H%M%S"))).unwrap_or(format!("")),
             )
             .with_extension(target.extension().and_then(|s| s.to_str()).unwrap_or(""));
 
-        self.set_status(&format!("Writing file {}", &out_file.display()), Color::BLACK);
-        std::fs::write(out_file, data)?;
+        self.set_status("Creating backup");
+        std::fs::rename(&target, &backup)?;
 
-        self.set_status(&format!("Successfully applied {} patches", patches.len()), Color::BLACK);
+        self.set_status(&format!("Writing file {}", &target.display()));
+        std::fs::write(target, data)?;
+
+        self.set_status(&format!("Successfully applied patch"));
+
+        msgbox::create(
+            "Patch successfull", 
+            &format!("The selected patch has been applied succesfully!"), 
+            msgbox::IconType::None,
+        )?;
 
         Ok(())
     }
@@ -125,12 +113,15 @@ impl Application for Patcher {
     type Flags = ();
 
     fn new(_flags: ()) -> (Self, Command<Message>) {
+        let patches = vec![
+            Patch::new("0gb", "Disable RAM check", include_bytes!("../patches/0gb.patch")),
+            Patch::new("remote_set", "Enable set command", include_bytes!("../patches/remote_set.patch")),
+        ];
+
         (
             Self {
-                patches: vec![
-                    (Patch::new("0gb", "Disable RAM check", include_bytes!("../patches/0gb.patch")), true),
-                    (Patch::new("remote_set", "Enable set command", include_bytes!("../patches/remote_set.patch")), true),
-                ],
+                selected_patch: patches.first().map(ToOwned::to_owned).expect("No patches available!"),
+                patches: patches,
                 ..Self::default()
             },
             Command::none(),
@@ -158,16 +149,17 @@ impl Application for Patcher {
             Message::FileSelectInputChanged(value) => {
                 self.target_path = value;
             },
-            Message::PatchSelected(id, selected) => {
-                for (patch, is_checked) in &mut self.patches {
-                    if *patch.id == id {
-                        *is_checked = selected;
-                    }
-                }
+            Message::PatchSelected(patch) => {
+                self.selected_patch = patch;
             },
             Message::PatchPressed => {
-                if let Err(e) = self.apply_patches() {
-                    self.set_status(&format!("{}", e.0), [1f32, 0f32, 0f32, 1f32].into());
+                if let Err(e) = self.apply_patch() {
+                    msgbox::create(
+                        "Error patching the executable", 
+                        &format!("An error occured while trying to patch the executable, please make sure you've selected the correct file!\n\n{}", &e.0), 
+                        msgbox::IconType::Error,
+                    ).expect("Error showing error message, what a pity...");
+                    self.set_status(&format!("{}", e.0));
                 }
             },
         }
@@ -176,39 +168,46 @@ impl Application for Patcher {
     }
 
     fn view(&mut self) -> Element<Message> {
-        let mut patches_container = Column::new();
+        let file_selector_input = TextInput::new(
+            &mut self.file_select_input,
+            "",
+            &self.target_path,
+            Message::FileSelectInputChanged,
+        );
 
-        for (patch, is_checked) in &self.patches {
-            let id = patch.id.clone();
-            patches_container =
-                patches_container.push(Checkbox::new(*is_checked, &patch.description, move |selected| {
-                    Message::PatchSelected(id.clone(), selected)
-                }));
-        }
+        let file_selector_button = Button::new(
+            &mut self.browse_button, 
+            Text::new("Browse")
+        )
+        .on_press(Message::BrowsePressed);
+
+        let patch_selector = PickList::new(&mut self.patch_selector, 
+            &self.patches, 
+            Some(self.selected_patch.clone()), 
+            Message::PatchSelected
+        );
+
+        let patch_button = Button::new(
+            &mut self.patch_button, Text::new("Patch")
+        )
+        .on_press(Message::PatchPressed);
 
         Container::new(
             Column::new()
-                .push(
-                    Row::new()
-                        .push(TextInput::new(
-                            &mut self.file_select_input,
-                            "RxGame-Win64-Test.exe",
-                            &self.target_path,
-                            Message::FileSelectInputChanged,
-                        ))
-                        .push(
-                            Button::new(&mut self.browse_button, Text::new("\u{2026}"))
-                                .on_press(Message::BrowsePressed),
-                        )
-                        .spacing(20),
+                .push(Text::new("File to patch:").width(Length::Fill))
+                .push(Row::new()
+                    .push(file_selector_input.padding(10).size(20))
+                    .push(file_selector_button.padding(10))
                 )
-                .push(patches_container)
-                .push(Button::new(&mut self.patch_button, Text::new("Patch")).on_press(Message::PatchPressed))
-                .push(Text::new(self.status_msg.clone()).color(self.status_color))
+                .push(Space::new(Length::Fill, 5.into()))
+                .push(patch_selector.width(Length::Fill))
+                .push(Space::new(Length::Fill, 5.into()))
+                .push(patch_button.padding(10))
                 .align_items(Align::Center)
-                .spacing(20)
+                .spacing(5)
                 .padding(20),
         )
+        .width(Length::Fill)
         .height(Length::Fill)
         .center_x()
         .center_y()
@@ -217,8 +216,13 @@ impl Application for Patcher {
 }
 
 pub fn run(_config: Config) {
+    if cfg!(windows) {
+        use winapi::um::wincon::{AttachConsole, ATTACH_PARENT_PROCESS}; 
+        unsafe { AttachConsole(ATTACH_PARENT_PROCESS); }
+    }
+
     Patcher::run(Settings {
-        window: Window { size: (300, 200), resizable: false, ..Window::default() },
+        window: Window { size: (300, 220), resizable: false, ..Window::default() },
         ..Settings::default()
     }).unwrap();
 }
